a81e2e8ab320b58495922837c08810ad24011a10
Adriano <adriano.cavalari@hotmail.com>
2018-01-09 16:51:02 +0000

    new 2

--- metacritic-webtest/server.js
+++ metacritic-webtest/server.js
@@ -1,8 +1,14 @@
 'use strict';
 var http = require('http');
+var metacritic = require('metacritic');
+
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+
+
+    var res = metacritic.escape('aaaaa&bbbb"cccc');
+
+    res.end(res);
 }).listen(port);
